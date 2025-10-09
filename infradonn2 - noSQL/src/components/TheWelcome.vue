acbda17041e85629c4c40ac0d44f162b6a25b042
lucaleone <lucaleone2004@gmail.com>
2025-10-09 11:58:31 +0200

    Add PouchDB and types to project dependencies
    Added pouchdb and @types/pouchdb to package.json and package-lock.json to enable NoSQL database functionality. This update prepares the project for PouchDB integration in the codebase.

--- infradonn2 - noSQL/src/components/TheWelcome.vue
+++ infradonn2 - noSQL/src/components/TheWelcome.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import PouchDB from 'pouchdb'
+
 // DATA VARIABLE
 const counter = ref(0)
 
 // FUNCTIONS / METHODS => CONTROLLER
+
+function initDatabase() {
+  const db = new PouchDB('http://admin:admin@localhost:5984/test_infradonn2')
+}
+
+function mounted() {
+  initDatabase()
+}
+
 const increment = () => {
   counter.value++
 }
